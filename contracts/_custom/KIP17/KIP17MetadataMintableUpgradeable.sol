25b1a3874114178c15cbd8b6ec05e9f0e89a5d1e
Namhoon Lee <frontalnh@gmail.com>
2022-04-27 22:52:15 +0900

    update

--- contracts/_custom/KIP17/KIP17MetadataMintableUpgradeable.sol
+++ contracts/_custom/KIP17/KIP17MetadataMintableUpgradeable.sol
@@ -31,21 +31,4 @@ contract KIP17MetadataMintableUpgradeable is KIP13Upgradeable, KIP17Upgradeable,
     __KIP17_init();
     __MinterRole_init();
   }
-
-  /**
-   * @dev Function to mint tokens.
-   * @param to The address that will receive the minted tokens.
-   * @param tokenId The token id to mint.
-   * @param tokenURI The token URI of the minted token.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mintWithTokenURI(
-    address to,
-    uint256 tokenId,
-    string memory tokenURI
-  ) public onlyMinter returns (bool) {
-    _mint(to, tokenId);
-    _setTokenURI(tokenId, tokenURI);
-    return true;
-  }
 }
