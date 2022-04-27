d0f362d3e35aa9dcb8f5c6acd8f7903cf31829ad
Namhoon Lee <frontalnh@gmail.com>
2022-04-27 15:59:29 +0900

    update

--- contracts/_custom/reveal/RevealableUpgradeable.sol
+++ contracts/_custom/reveal/RevealableUpgradeable.sol
@@ -1,16 +1,11 @@
-import "../ownership/OwnableUpgradeable.sol";
 pragma solidity ^0.5.6;
 
-contract RevealableUpgradeable is OwnableUpgradeable {
+contract RevealableUpgradeable {
   bool public revealed = false;
   string internal _revealURI;
 
   function reveal() public {
     revealed = true;
-  }
-
-  function __Revealable_init() public {
-    __Ownable_init();
   }
 
   function setRevealURI(string memory uri) public {
