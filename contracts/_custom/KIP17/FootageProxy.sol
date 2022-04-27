ca1ec57334808f547c6c7576f1e9e3ba950e27e6
Namhoon Lee <frontalnh@gmail.com>
2022-04-27 12:53:46 +0900

    update

--- contracts/_custom/KIP17/FootageProxy.sol
+++ contracts/_custom/KIP17/FootageProxy.sol
@@ -4,7 +4,7 @@ import "../../ownership/Ownable.sol";
 import "../../token/KIP17/IKIP17.sol";
 import "../oldproxy/TransparentUpgradeableProxy.sol";
 
-contract LemongBiddingProxy is TransparentUpgradeableProxy, Ownable {
+contract FootageProxy is TransparentUpgradeableProxy, Ownable {
   constructor(address _logic, address _admin) public TransparentUpgradeableProxy(_logic, _admin, "") {}
 
   // Storage Start
