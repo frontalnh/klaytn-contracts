1fe55631c2275a600eb6497ea8cadd83ffe86ec1
Namhoon Lee <frontalnh@gmail.com>
2022-04-27 13:11:07 +0900

    update

--- contracts/_custom/KIP17/FootageProxy.sol
+++ contracts/_custom/KIP17/FootageProxy.sol
@@ -6,36 +6,4 @@ import "../oldproxy/TransparentUpgradeableProxy.sol";
 
 contract FootageProxy is TransparentUpgradeableProxy, Ownable {
   constructor(address _logic, address _admin) public TransparentUpgradeableProxy(_logic, _admin, "") {}
-
-  // Storage Start
-  uint256 public _maxPerAddressDuringMint;
-  uint256 public amountForDevs;
-  struct PreSaleConf {
-    bool open;
-    uint32 startTime;
-    uint32 endTime;
-    uint256 price;
-    uint256 limit;
-  }
-  struct PublicSaleConf {
-    bool open;
-    uint32 publicSaleKey;
-    uint32 startTime;
-    uint32 endTime;
-    uint64 price;
-    uint256 limit;
-  }
-  struct AllowSaleConf {
-    bool open;
-    uint256 price; // mint price for allow list accounts
-    uint32 startTime;
-    uint32 endTime;
-    mapping(address => uint256) allowlist;
-    uint256 limit;
-  }
-  PreSaleConf public preSaleConf;
-  PublicSaleConf public publicSaleConf;
-  AllowSaleConf public allowSaleConf;
-
-  // Storage End
 }
