21679227ed1297a6a7ef87557b849a5b0ae06a34
Namhoon Lee <frontalnh@gmail.com>
2022-04-27 23:03:13 +0900

    update

--- contracts/_custom/KIP17/KIP17TokenAUpgradeable.sol
+++ contracts/_custom/KIP17/KIP17TokenAUpgradeable.sol
@@ -5,18 +5,17 @@ import "./KIP17BurnableUpgradeable.sol";
 import "./KIP17EnumerableUpgradeable.sol";
 import "./KIP17Upgradeable.sol";
 import "./KIP17EnumerableUpgradeable.sol";
-import "./KIP17MetadataUpgradeable.sol";
 
-contract KIP17TokenAUpgradeable is
-  KIP17Upgradeable,
-  KIP17EnumerableUpgradeable,
-  KIP17MetadataUpgradeable,
-  KIP17MintableUpgradeable,
-  KIP17BurnableUpgradeable
-{
+contract KIP17TokenAUpgradeable is KIP17Upgradeable, KIP17EnumerableUpgradeable, KIP17MintableUpgradeable, KIP17BurnableUpgradeable {
+  // Token name
+  string private _name;
+
+  // Token symbol
+  string private _symbol;
   uint256 private currentIndex = 0;
   uint256 internal collectionSize;
   uint256 internal maxBatchSize;
+  bytes4 private constant _INTERFACE_ID_KIP17_METADATA = 0x5b5e139f;
 
   function __KIP17TokenA_init(
     string memory name,
@@ -26,7 +25,12 @@ contract KIP17TokenAUpgradeable is
   ) internal {
     __KIP17_init();
     __KIP17Enumerable_init();
-    __KIP17Metadata_init(name, symbol);
+    _name = name;
+    _symbol = symbol;
+
+    // register the supported interfaces to conform to KIP17 via KIP13
+    _registerInterface(_INTERFACE_ID_KIP17_METADATA);
+    __KIP13_init();
     __KIP17Mintable_init();
     __KIP17Burnable_init();
     maxBatchSize = maxBatchSize_;
@@ -60,5 +64,21 @@ contract KIP17TokenAUpgradeable is
     }
 
     currentIndex = tokenId;
+  }
+
+  /**
+   * @dev Gets the token name.
+   * @return string representing the token name
+   */
+  function name() external view returns (string memory) {
+    return _name;
+  }
+
+  /**
+   * @dev Gets the token symbol.
+   * @return string representing the token symbol
+   */
+  function symbol() external view returns (string memory) {
+    return _symbol;
   }
 }
