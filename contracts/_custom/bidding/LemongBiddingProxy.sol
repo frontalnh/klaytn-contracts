dfe7f9e0873e7a648b95a369b122ed0eb60af5e9
Namhoon Lee <frontalnh@gmail.com>
2022-04-30 12:28:30 +0900

    update

--- contracts/_custom/bidding/LemongBiddingProxy.sol
+++ contracts/_custom/bidding/LemongBiddingProxy.sol
@@ -4,6 +4,6 @@ import "../../ownership/Ownable.sol";
 import "../../token/KIP17/IKIP17.sol";
 import "../oldproxy/TransparentUpgradeableProxy.sol";
 
-contract LemongBiddingProxy is TransparentUpgradeableProxy, Ownable {
+contract LemongBiddingProxy is TransparentUpgradeableProxy {
   constructor(address _logic, address _admin) public TransparentUpgradeableProxy(_logic, _admin, "") {}
 }
