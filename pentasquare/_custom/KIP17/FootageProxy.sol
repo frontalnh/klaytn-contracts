7e4da545fd2cd8e58beef9741bfeee825dd6d7ee
Namhoon Lee <frontalnh@gmail.com>
2022-05-03 17:04:34 +0900

    update

--- pentasquare/_custom/KIP17/FootageProxy.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.6;
-
-import "../../token/KIP17/IKIP17.sol";
-import "../oldproxy/TransparentUpgradeableProxy.sol";
-
-contract FootageProxy is TransparentUpgradeableProxy {
-  constructor(address _logic, address _admin) public TransparentUpgradeableProxy(_logic, _admin, "") {}
-}
